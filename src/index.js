a9c1e699a14f2cc4517e5fc293fccf62f0d4c064
Hamed Ali <123hamed@gmail.com>
2018-10-15 20:52:58 +1300

    ADD redux persist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store';
+import { store, persistor } from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -1,18 +1,33 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './reducers';
 import rootSaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware();
 
+// Redux persist config
+const config = {
+  key: 'primary',
+  storage,
+  whitelist: [
+    'authentication'
+  ]
+}
+
+const reducer = persistReducer(config, rootReducer);
+
 // redux devtools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(
+const store = createStore(reducer, composeEnhancers(
   applyMiddleware(sagaMiddleware)
 ));
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+const persistor = persistStore(store);
+
+export { persistor, store };
