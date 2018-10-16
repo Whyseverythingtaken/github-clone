35cc881b6d04c5739342b24d72cffd2ff43af87f
Hamed Ali <123hamed@gmail.com>
2018-10-16 23:53:07 +1300

    REMOVE unused import

--- src/scenes/Root/index.js
+++ src/scenes/Root/index.js
@@ -7,7 +7,6 @@ import { Route } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 
 // Scenes
 import Login from '../Login';
